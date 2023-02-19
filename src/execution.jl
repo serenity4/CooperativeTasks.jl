b9d7a938762da2383941889854c0c63009ed3b29
serenity4 <cedric.bel@hotmail.fr>
2023-02-19 14:23:39 +0100

    Define `spawn` as a function, add more options, improve documentation

--- src/execution.jl
+++ src/execution.jl
@@ -22,6 +22,12 @@ function record_activity(f, state::ExecutionState)
   timed.value
 end
 
+function has_activity(state::ExecutionState)
+  isempty(state.recent_activity) && return false
+  time() - last(state.recent_activity).time < 3exec.period
+end
+
+"Execute a function once, then return."
 struct SingleExecution <: ExecutionMode end
 
 function (exec::SingleExecution)(f = Returns(nothing))
@@ -31,6 +37,19 @@ function (exec::SingleExecution)(f = Returns(nothing))
   end
 end
 
+"""
+Execute a function repeatedly until a shutdown is scheduled with [`schedule_shutdown`](@ref).
+
+If `period` is greater than a millisecond, every iteration may trigger a sleep
+for the remaining period time after executing the main function.
+Simple heuristics are used to prevent sleeping when there were recent interactions
+with other tasks, to avoid suffering from large communication delays.
+
+At every iteration, task messages will be checked and allow (among other things)
+to compute/return results to other tasks (see [`Future`](@ref)), and to cancel the task.
+This mode of execution is preferred over manual loops precisely for the ability to satisfy
+task duties as required in the context of this library.
+"""
 struct LoopExecution <: ExecutionMode
   period::Union{Nothing,Float64}
   state::ExecutionState
@@ -63,7 +82,7 @@ function (exec::LoopExecution)(f = Returns(nothing))
         shutdown_scheduled() && @goto out
 
         Δt = time() - t0
-        if Δt - exec.period ≥ 0.001 && !has_activity(exec)
+        if Δt - exec.period ≥ 0.001 && !has_activity(exec.state)
           sleep(Δt - exec.period)
         else
           yield()
@@ -78,15 +97,11 @@ function (exec::LoopExecution)(f = Returns(nothing))
 end
 
 function shutdown()
+  schedule_shutdown() # in case it was not already set as scheduled
   shutdown_children()
   for t in known_tasks()
     state(t) ≠ DEAD && signal_shutdown(t)
   end
-end
-
-function has_activity(exec::ExecutionMode)
-  isempty(exec.state.recent_activity) && return false
-  time() - last(exec.state.recent_activity).time < 3exec.period
 end
 
 signal_shutdown(task::Task) = tryexecute(set_task_state, task, current_task(), DEAD)
--- src/spawn.jl
+++ src/spawn.jl
@@ -1,28 +1,54 @@
+"""
+Options for spawning tasks with [`spawn`](@ref).
+"""
+Base.@kwdef struct SpawnOptions
+  """
+  [`ExecutionMode`](@ref) used for running the task.
+
+  See [`LoopExecution`](@ref), [`SingleExecution`](@ref)
+  """
+  execution_mode::Union{LoopExecution,SingleExecution} = SingleExecution()
+  """
+  Optional thread ID to start the task on.
+
+  If task migration is disabled, then the task will execute on this thread during its entire lifespan.
+  If set to `nothing` (default), then the starting thread is the same as the thread launching the task.
+
+  !!! warning
+      If task migration is disabled, and no start thread ID has been set, the task will run similarly to an `@async`-spawned
+      task and execution will never occur in parallel between the spawning task and the spawned task.
+  """
+  start_threadid::Union{Nothing,Int} = nothing
+  """
+  Allow tasks to migrate between Julia threads.
+
+  This makes it reliable to use `Threads.threadid()` as an index from this task, for example enabling the pattern
+
+  ```julia
+  const results = [Int[] for i in 1:Threads.nthreads()]
+
+  # ...
+  # Spawn a bunch of tasks.
+  # ...
+  # Execute code like the following from these tasks:
+  push!(results[Threads.threadid()], rand())
+  ```
+
+  Disabling this can be useful when e.g. C libraries rely on functions to be executed in the same thread in which
+  some library-defined context has been created, as can be the case for graphics API such as Vulkan or OpenGL.
+  """
+  allow_task_migration::Bool = true
+end
+
+"Variable used for `Base.@sync` blocks."
+const SYNC_VARNAME = Base.sync_varname
+
 macro spawn(mode, ex)
-  sync_var = esc(Base.sync_varname)
   quote
-    exec = execution_mode($(esc(mode)))
-    task = Task(exec(() -> $(esc(ex))))
-
-    # Note: this is not concurrent, be careful to not schedule the task for execution before we are done with `tls`.
-    tls = Base.get_task_tls(task)
-    tls[:task_owner] = current_task()
-    set!(error_handlers(), task, throw)
-    tls[:mpi_channel] = Channel{Message}(Inf)
-    tls[:mpi_task_states] = dictionary([current_task() => ALIVE])
-    set_task_state(task, ALIVE)
-
-    # List spawned task as child of the current task.
-    push!(children_tasks(), task)
-
-    # Allow task to run on any OS thread.
-    task.sticky = false
-
-    # Support use with `@sync` blocks.
-    $(Expr(:islocal, sync_var)) && put!($sync_var, task)
-
-    schedule(task)
-    task
+    options = SpawnOptions(
+      execution_mode = execution_mode($(esc(mode))),
+    )
+    spawn(() -> $(esc(ex)), options)
   end
 end
 
@@ -30,3 +56,32 @@ macro spawn(ex) :(@spawn :single $(esc(ex))) end
 
 execution_mode(mode::Symbol) = mode == :single ? SingleExecution() : mode == :looped ? LoopExecution(nothing) : error("Unknown execution mode '$mode'")
 execution_mode(mode::ExecutionMode) = mode
+
+"""
+Spawn the function `f` on a new task.
+
+See also: [`SpawnOptions`](@ref)
+"""
+function spawn(f, options::SpawnOptions)
+  task = Task(options.execution_mode(f))
+
+  # Note: this is not concurrent, be careful to not schedule the task for execution before we are done with `tls`.
+  tls = Base.get_task_tls(task)
+  tls[:task_owner] = current_task()
+  set!(error_handlers(), task, throw)
+  tls[:mpi_channel] = Channel{Message}(Inf)
+  tls[:mpi_task_states] = dictionary([current_task() => ALIVE])
+  set_task_state(task, ALIVE)
+
+  # List spawned task as child of the current task.
+  push!(children_tasks(), task)
+
+  task.sticky = !options.allow_task_migration
+  !isnothing(options.start_threadid) && ccall(:jl_set_task_tid, Cvoid, (Any, Cint), task, options.start_threadid)
+
+  # Support use with `@sync` blocks.
+  @eval $(Expr(:islocal, SYNC_VARNAME)) && put!($SYNC_VARNAME, task)
+
+  schedule(task)
+  task
+end
